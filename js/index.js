c23eb51e548137c508c4a0035bafcaf01d166b9c
kmbhuvanprasad <dshivakumar@qwinix.io>
2018-03-22 17:32:45 +0530

    updated assment 2

---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	// $(window).scroll(function(){
+	// 	if ($(this).scrollTop() > 100) {
+	// 		$('.scrollToTop').fadeIn();
+	// 	} else {
+	// 		$('.scrollToTop').fadeOut();
+	// 	}
+	// });
+	// $('.scrollToTop').click(function(){
+	// 	$('html, body').animate({scrollTop : 0},800);
+	// 	return false;
+	// });
+	$("#about").hide();
+	$("#hello").click(function(){
+		$("#home").hide();
+		$("#about").show();
+	});
+	$("#hi").click(function(){
+		$("#about").hide();
+		$("#home").show();
+});
+	$("#about").hide();
+	$("#step1").click(function(){
+		$("#home").hide();
+		$("#about").show();
+	});
+	$("#about").hide();
+	$("#step2").click(function(){
+		$("#home").hide();
+		$("#about").show();
+	});
+	$("#about").hide();
+	$("#step3").click(function(){
+		$("#home").hide();
+		$("#about").show();
+	});
+});
+
+
