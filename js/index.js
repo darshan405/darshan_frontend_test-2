557f44bea6d39e45bfbc8120f971cfb7a887c053
kmbhuvanprasad <dshivakumar@qwinix.io>
2018-03-23 17:57:39 +0530

    second updated assment 2

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-	// $(window).scroll(function(){
-	// 	if ($(this).scrollTop() > 100) {
-	// 		$('.scrollToTop').fadeIn();
-	// 	} else {
-	// 		$('.scrollToTop').fadeOut();
-	// 	}
-	// });
-	// $('.scrollToTop').click(function(){
-	// 	$('html, body').animate({scrollTop : 0},800);
-	// 	return false;
-	// });
 	$("#about").hide();
 	$("#hello").click(function(){
 		$("#home").hide();
@@ -18,7 +7,7 @@ $(document).ready(function(){
 	$("#hi").click(function(){
 		$("#about").hide();
 		$("#home").show();
-});
+	});
 	$("#about").hide();
 	$("#step1").click(function(){
 		$("#home").hide();
@@ -33,6 +22,28 @@ $(document).ready(function(){
 	$("#step3").click(function(){
 		$("#home").hide();
 		$("#about").show();
+	});
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	$("#about").hide();
+	$("#qwerty").click(function(){
+		$("#home").hide();
+		$("#about").show();
+	});
+	
+	$("#about").hide();
+	$("#logo").click(function(){
+		$("#home").show();
+		$("#about").hide();
 	});
 });
 
